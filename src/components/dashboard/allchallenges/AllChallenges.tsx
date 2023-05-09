46ac1c218cde2e8a4eddaa9aa18ed53ec010913a
ZaxtuN <898602+ZaxtuN@users.noreply.github.com>
2023-05-09 18:05:55 -0400

    File structuring

---
+++ src/components/dashboard/allchallenges/AllChallenges.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AllChallenges = () => {
+  return (
+    <div>AllChallenges</div>
+  )
+}
+
+export default AllChallenges
---
+++ src/components/dashboard/singlechallenge/SingleChallenge.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SingleChallenge = () => {
+  return (
+    <div>SingleChallenge</div>
+  )
+}
+
+export default SingleChallenge
