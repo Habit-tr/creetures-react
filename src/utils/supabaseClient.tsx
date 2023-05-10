d6940672bf31b3b838f8fe115aec8cd540facb81
ZaxtuN <898602+ZaxtuN@users.noreply.github.com>
2023-05-10 12:04:05 -0400

    Working ENV file

--- src/utils/supabaseClient.tsx
+++ src/utils/supabaseClient.tsx
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 export default createClient(supabaseUrl!, supabaseKey!);
