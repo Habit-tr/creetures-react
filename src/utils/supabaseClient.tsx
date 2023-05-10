c052b454a249c9a887ee6d477803a49b6b838ad8
ZaxtuN <898602+ZaxtuN@users.noreply.github.com>
2023-05-10 12:13:17 -0400

    Added dotenv working

--- src/utils/supabaseClient.tsx
+++ src/utils/supabaseClient.tsx
@@ -2,5 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+console.log(supabaseUrl);
+console.log(supabaseKey);
 
 export default createClient(supabaseUrl!, supabaseKey!);
