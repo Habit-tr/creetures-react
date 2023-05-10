78ca705f973f83819225c4ab5322f8f2928cda7f
jackconwayiv <jackconwayiv@gmail.com>
2023-05-10 13:22:09 -0700

    post new challenge works without errors

--- src/components/dashboard/challenges/AllChallenges.tsx
+++ src/components/dashboard/challenges/AllChallenges.tsx
@@ -1,6 +1,7 @@
 import { Button, Flex, Heading, useDisclosure } from "@chakra-ui/react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../../../utils/reduxHooks";
+import { Challenge } from "../../../utils/supabaseTypes";
 import AddChallenge from "./AddChallenge";
 import ChallengeCard from "./ChallengeCard";
 import {
@@ -9,30 +10,30 @@ import {
 } from "./allChallengesSlice";
 
 const AllChallenges = () => {
-  // const [challenges, setChallenges] = useState<any[]>([]);
+  const [challenges, setChallenges] = useState<Challenge[]>([]);
   const { isOpen, onOpen, onClose } = useDisclosure();
   const dispatch = useAppDispatch();
 
-  useEffect(() => {
-    // const { data } = await supabase.from("challenges").select(); //this is moving to the reducer
-    dispatch(fetchAllChallengesAsync());
-  }, [dispatch]);
+  const fetchedChallenges = useAppSelector(selectChallenges);
 
-  const challenges = useAppSelector(selectChallenges);
+  useEffect(() => {
+    dispatch(fetchAllChallengesAsync());
+    setChallenges(fetchedChallenges);
+  }, [dispatch, fetchedChallenges]);
 
   return (
     <>
       <Heading>All Challenges</Heading>
-      {challenges && challenges.length ? (
-        <Flex direction="row" maxW="900px" wrap="wrap">
-          {challenges.map((challenge) => {
-            return <ChallengeCard key={challenge.id} challenge={challenge} />;
-          })}
-        </Flex>
-      ) : null}
       <Button bgColor="purple.200" onClick={onOpen}>
         Create Challenge
       </Button>
+      {challenges && challenges.length ? (
+        <Flex direction="row" maxW="900px" wrap="wrap">
+          {challenges.map((challenge, id) => {
+            return <ChallengeCard key={id} challenge={challenge} />;
+          })}
+        </Flex>
+      ) : null}
       <AddChallenge isOpen={isOpen} onClose={onClose} />
     </>
   );
