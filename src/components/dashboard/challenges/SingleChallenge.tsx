9057b78afb5c11ee4000744368763f58e2d1cf22
Danny DiDomenico <daniel.p.didomenico@gmail.com>
2023-05-10 09:02:32 -0400

    Added AllCategories and SingleCategory components, fleshed out AllChallenges and SingleChallenge

--- src/components/dashboard/challenges/SingleChallenge.tsx
+++ src/components/dashboard/challenges/SingleChallenge.tsx
@@ -1,9 +1,36 @@
-import { Heading } from "@chakra-ui/react";
-import { useParams } from "react-router";
+import { useState, useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import supabase from '../../../utils/supabaseClient';
+import { Button, Heading, Text } from '@chakra-ui/react';
 
 const SingleChallenge = () => {
   const { id } = useParams();
-  return <Heading>Single Challenge for #{id}</Heading>;
+  const [challenge, setChallenge] = useState<any>({});
+  const [category, setCategory] = useState<string>('');
+  
+  useEffect(() => {
+    async function fetchSingleChallenge(id: string) {
+      const { data: fetchedChallenge } = await supabase.from('challenges').select().match({ id }).single();
+      setChallenge(fetchedChallenge);
+      const res = await supabase.from('categories').select('name').eq('id', fetchedChallenge?.category_id).single();
+      setCategory(res.data?.name ?? '');
+    }
+    fetchSingleChallenge(id!);
+  }, [id]);
+
+  return (
+    challenge && category
+    ? <>
+        <Heading>{challenge.name}</Heading>
+        <Text>
+          Category:&nbsp;&nbsp;
+          <Link to={`/challenges/categories/${category}`}>{category}</Link>  
+        </Text>
+        <Button>Commit to this Challenge</Button>
+        {/* "Commit" button links to commit form modal */}
+      </>
+    : null
+  );
 };
 
 export default SingleChallenge;
