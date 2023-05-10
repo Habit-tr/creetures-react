c548cfbbfb18373a45be5c11cdaaf686e1e4eea1
ZaxtuN <898602+ZaxtuN@users.noreply.github.com>
2023-05-10 11:19:16 -0400

    Changed supabase key

--- src/utils/supabaseClient.tsx
+++ src/utils/supabaseClient.tsx
@@ -1,8 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://xrinzjsqsfarjyupbckx.supabase.co";
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
 
-export default createClient(
-  supabaseUrl,
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhyaW56anNxc2Zhcmp5dXBiY2t4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMyMTY0MTUsImV4cCI6MTk5ODc5MjQxNX0.djAf1T_OKYaWMRQ1tJIDIVbXyu8jfdZnit_TmoBujXo'
-);
+export default createClient(supabaseUrl!, supabaseKey!);
